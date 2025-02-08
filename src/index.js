791e94ced5f7c29ce997943924707585174aa22d
Hebbar <prajwalhebbaras@gmail.com>
2025-02-08 14:22:14 +0530

    function as a reference

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Redux Starter Project!!");
+function greetings() {
+  return "Good Afternoon!";
+}
+
+// Pass the reference to the function to the message variable and hence the message will also become a function.
+const message = greetings;
+
+// As you can see in these console logs although we are calling 2 different functions both return the same value since the message has a reference to the function greetings.
+console.log("Message Function: ", message());
+console.log("Greetings Function: ", greetings());
