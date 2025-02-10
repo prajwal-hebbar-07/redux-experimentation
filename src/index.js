e38988632e4ce99e7a374b73909200d535c5b818
Hebbar <prajwalhebbaras@gmail.com>
2025-02-10 14:43:43 +0530

    use immer library to update the immutable object

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-// How to update Immutable Objects.
+// import the produce function from the immer library.
+
+import { produce } from "immer";
 
 const person = {
-  name: "Prajwal",
-  age: 25,
-};
-
-// First method is to use Object.assign function to do this update.
-const newPerson = Object.assign({}, person, { name: "Hebbar" });
-
-console.log("Person: ", person);
-console.log("New Person: ", newPerson);
-
-const employee = {
-  name: "Prajwal",
+  name: "Hebbar",
   age: 25,
   place: {
     country: "India",
@@ -20,19 +11,11 @@ const employee = {
   },
 };
 
-// Second method is to use spread operator to update the object.
-const newEmployee = {
-  ...employee,
-  name: "Hebbar",
-  // To avoid updating the original object we need to use spread operator here as well.
-  place: {
-    ...employee.place,
-    state: "Delhi",
-  },
-};
+// Previously we used different methods to update the objects in a immutable way and here we are going to use a library called as immer to do the job since Redux by default under the hood uses this library to do the task.
 
-// If we try to update the newEmployee Object directly without spread operator it will also change the employee object since the reference for the employee object is stored.
-// newEmployee.place.state = "Delhi";
+const newPerson = produce(person, (prevState) => {
+  (prevState.name = "Prajwal"), (prevState.place.state = "Delhi");
+});
 
-console.log("Employee: ", employee);
-console.log("New Employee: ", newEmployee);
+console.log("Person: ", person);
+console.log("New person: ", newPerson);
