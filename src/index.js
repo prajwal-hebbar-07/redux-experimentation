ba0468ac7d1f43fb3da2c427472ead9f677bd0b1
Hebbar <prajwalhebbaras@gmail.com>
2025-02-10 20:21:28 +0530

    immutable array methods for multiple operations

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-// import the produce function from the immer library.
+// Immutable Arrays
 
-import { produce } from "immer";
+const numbers = [10, 20, 30, 40, 50];
 
-const person = {
-  name: "Hebbar",
-  age: 25,
-  place: {
-    country: "India",
-    state: "Karnataka",
-  },
-};
+// Adding items to the array can be done using the spread operator. If we want to add an element in the begging then start with the value and then use spread operator. Do the vice versa if you want to add the value in the end.
+const newNumbers = [50, ...numbers];
 
-// Previously we used different methods to update the objects in a immutable way and here we are going to use a library called as immer to do the job since Redux by default under the hood uses this library to do the task.
+console.log("Numbers: ", numbers);
+console.log("New Numbers: ", newNumbers);
 
-const newPerson = produce(person, (prevState) => {
-  (prevState.name = "Prajwal"), (prevState.place.state = "Delhi");
-});
+// Adding a value in the middle of the array when we know after which element we need to add the value. For this we need to use slice and not splice.
+const index = numbers.indexOf(30);
+const midNumbers = [...numbers.slice(0, index), 80, ...numbers.slice(index)];
 
-console.log("Person: ", person);
-console.log("New person: ", newPerson);
+console.log("Mid Numbers: ", midNumbers);
+
+// Updating an item in the array. For this we use the array map method.
+const updatedNumbers = numbers.map((number) => (number === 30 ? 80 : number));
+
+console.log("Updated Numbers: ", updatedNumbers);
+
+// Removing an item from the array. For this we will use the array filter method.
+const removedNumbers = numbers.filter((number) => number !== 40);
+
+console.log("Removed Numbers: ", removedNumbers);
