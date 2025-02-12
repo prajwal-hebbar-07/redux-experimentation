bba4ddb5e1859b78fad1d32d20ff280fed401d2d
Hebbar <prajwalhebbaras@gmail.com>
2025-02-12 11:50:41 +0530

    udpate the actions object for different actions

--- src/index.js
+++ src/index.js
@@ -5,3 +5,20 @@ const store = [
     completed: false,
   },
 ];
+
+// Add Actions
+
+// We use payload so that the object for action will always be the same and key value pairs won't change on a parent level
+const addTaskAction = {
+  type: "ADD_TASK",
+  payload: {
+    task: "This is new Task!",
+  },
+};
+
+const removeTaskAction = {
+  type: "REMOVE_TASK",
+  payload: {
+    id: 1,
+  },
+};
