1fbe719f1506ddaa1f9ebd9b6a97e052d5c2d715
Hebbar <prajwalhebbaras@gmail.com>
2025-02-12 13:46:09 +0530

    update logic for creating a redux store

--- src/index.js
+++ src/index.js
@@ -1,24 +1,3 @@
-const store = [
-  {
-    id: 1,
-    task: "This is the first task",
-    completed: false,
-  },
-];
+import store from "./store";
 
-// Add Actions
-
-// We use payload so that the object for action will always be the same and key value pairs won't change on a parent level
-const addTaskAction = {
-  type: "ADD_TASK",
-  payload: {
-    task: "This is new Task!",
-  },
-};
-
-const removeTaskAction = {
-  type: "REMOVE_TASK",
-  payload: {
-    id: 1,
-  },
-};
+console.log(store.getState());
--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
 let id = 0;
 
-function reducer(state = [], action) {
+export default function reducer(state = [], action) {
   switch (action.type) {
     case "ADD_TASK":
       return [
