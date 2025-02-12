6e8ff7932c5644d769613ffc13af1d599297853a
Hebbar <prajwalhebbaras@gmail.com>
2025-02-12 14:12:47 +0530

    udpate action types

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,10 @@
+import * as actionTypes from "./actionTypes";
+
 let id = 0;
 
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case "ADD_TASK":
+    case actionTypes.ADD_TASK:
       return [
         ...state,
         {
@@ -12,7 +14,7 @@ export default function reducer(state = [], action) {
         },
       ];
 
-    case "REMOVE_TASK":
+    case actionTypes.REMOVE_TASK:
       return state.filter((task) => task.id !== action.payload.id);
 
     default:
