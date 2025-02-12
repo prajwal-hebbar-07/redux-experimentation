b13a5fc3f9fd06fa375e600e78d5dccf9268eb65
Hebbar <prajwalhebbaras@gmail.com>
2025-02-12 14:18:29 +0530

    subscribe and unsubscribe in redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import { addTask, removeTask } from "./action";
 import store from "./store";
 
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated State: ", store.getState());
+});
+
 //store.dispatch({ type: "ADD_TASK", payload: { task: "Task 1" } });
 // Since the dispatch method is big we need to create an action function which can be used to update the store.
 store.dispatch(addTask("Task 1"));
 console.log(store.getState());
+
+unsubscribe();
 
 store.dispatch(removeTask(1));
 console.log(store.getState());
