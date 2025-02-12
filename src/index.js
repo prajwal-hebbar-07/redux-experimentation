46cca6f7aeda80d8c7d26332f5c4b2905ff9640c
Hebbar <prajwalhebbaras@gmail.com>
2025-02-12 11:48:21 +0530

    design a store

--- src/index.js
+++ src/index.js
@@ -1,28 +1,7 @@
-// Immutability exercise
-
-import { produce } from "immer";
-
-// Change the price and rating using immutable object
-const book = {
-  author: "Robert Kiyosaki",
-  book: {
-    name: "Rich Dad Poor Dad",
-    price: "$7",
-    rating: 4.7,
+const store = [
+  {
+    id: 1,
+    task: "This is the first task",
+    completed: false,
   },
-};
-
-const updatedBook = produce(book, (prevBook) => {
-  (prevBook.book.price = "$10"), (prevBook.book.rating = 4.8);
-});
-
-console.log("Book: ", book);
-console.log("Updated Book: ", updatedBook);
-
-// Change Book2 to Book4 using immutable arrays
-const books = ["Book1", "Book2", "Book3"];
-
-const updatedBooks = books.map((book) => (book === "Book2" ? "Book4" : book));
-
-console.log("Books: ", books);
-console.log("Updated Books: ", updatedBooks);
+];
