767007e17c51a4c3c4c5690e2db69f0eed613992
Hebbar <prajwalhebbaras@gmail.com>
2025-02-12 12:04:20 +0530

    reducer function using switch statement

--- src/reducer.js
+++ src/reducer.js
@@ -1,18 +1,21 @@
 let id = 0;
 
 function reducer(state = [], action) {
-  if (action.type === "ADD_TASK") {
-    return [
-      ...state,
-      {
-        id: ++id,
-        task: action.payload.task,
-        completed: false,
-      },
-    ];
-  } else if (action.type === "REMOVE_TASK") {
-    return state.filter((task) => task.id !== action.payload.id);
-  }
+  switch (action.type) {
+    case "ADD_TASK":
+      return [
+        ...state,
+        {
+          id: ++id,
+          task: action.payload.task,
+          completed: false,
+        },
+      ];
 
-  return state;
+    case "REMOVE_TASK":
+      return state.filter((task) => task.id !== action.payload.id);
+
+    default:
+      return state;
+  }
 }
